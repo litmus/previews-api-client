f74631c4631485b48989309894f5b6f67dc029d3
Ed Giardina <ed@litmus.com>
2016-07-01 13:05:54 -0400

    committing new nuget package

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Litmus.PreviewsApi")]
 [assembly: AssemblyDescription("Litmus Previews Api Library")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Litmus")]
+[assembly: AssemblyCompany("Litmus Software")]
 [assembly: AssemblyProduct("Litmus.PreviewsApi")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright © Litmus Software 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
